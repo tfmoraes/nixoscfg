0d8e07aad11f248b7b54b998f32e0bf6172b1bb8
Thiago Franco de Moraes <totonixsame@gmail.com>
2020-11-02 16:43:26 -0300

    Home-manager following nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,15 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs = { self, home-manager, nixpkgs, ... }: rec{
